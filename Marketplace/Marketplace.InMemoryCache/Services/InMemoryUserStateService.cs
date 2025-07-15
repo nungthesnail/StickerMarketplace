1cde8dbd018714586653e4a14b4bc145767b7813
Alexander Zakharchuk <nungthesnail@mail.ru>
2025-07-15 17:52:20 +0500

    Реализация сервиса для хранения состояний пользователей

---
+++ Marketplace/Marketplace.InMemoryCache/Services/InMemoryUserStateService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Concurrent;
+using Marketplace.Core.Abstractions.Services;
+using Marketplace.Core.Models.UserStates;
+
+namespace Marketplace.InMemoryCache.Services;
+
+public class InMemoryUserStateService : IUserStateService
+{
+    private readonly ConcurrentDictionary<long, UserState> _statesStorage = new();
+    
+    public UserState? GetUserState(long userId)
+        => _statesStorage.GetValueOrDefault(userId);
+    public void SetUserState(long userId, UserState state)
+        => _statesStorage.AddOrUpdate(userId, state, (_, _) => state);
+    public void RemoveUserState(long userId)
+        => _statesStorage.TryRemove(userId, out _);
+}
